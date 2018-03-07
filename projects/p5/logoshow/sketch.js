464381c3c2bdcd87cc1bd30d413c57b9c3957ecf
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-03-07 23:32:36 +1100

    testing logo art

--- projects/p5/logoshow/sketch.js
+++ projects/p5/logoshow/sketch.js
@@ -1,29 +1,40 @@
-var image;
-var w = 1000;
-var h = 1000;
+var img;
+var w = 500;
+var h = 500;
 var timer = new Date();
+var points = [];
 
 function preload(){
-  image = loadImage("assets/icon.png");
+  img = loadImage("assets/icon.png");
 }
 
 function setup() {
-  createCanvas(1000,1000);
+  createCanvas(w,h);
   imageMode(CENTER);
-  image.loadPixels();
+  img.loadPixels();
 }
 
 function draw() {
+  background(0);
   frameRate(60);
 
-
-  var now = new Date();
-  if((now.getTime() - timer.getTime()) >= 100){
-
+  if(points.length < 500){
+    //var now = new Date();
+  for (var i = 0; i < 20; i++) {
+    //if((now.getTime() - timer.getTime()) >= 50){
+      //timer = new Date();
+      var newX = random(0,w);
+      var newY = random(0,h);
+      var newPoint = new Point(newX,newY);
+      points.push(newPoint);
+      //print('1');
+    //}
+  }
   }
 
   for (var i = 0; i < points.length; i++) {
     points[i].grow();
+    //print(points[i].rad);
     points[i].show();
   }
 }
@@ -31,15 +42,38 @@ function draw() {
 function Point(x,y){
   this.x = x;
   this.y = y;
-  this.speed = random(0,5);
-  this.rad = 0
+  this.speed = random(0.1,5);
+  this.rad = 0;
+  this.canGrow = true;
+  this.color = random(10,255);
+  this.color = img.get(this.x,this.y);
   this.grow = function(){
-    this.rad = this.rad + this.speed;
+    this.canGrow = check(this);
+    if(this.canGrow){
+      this.rad = this.rad + this.speed;
+    }
   }
 
-  this.draw = function(){
-    var color = image.get(this.x,this.y);
-    fill(color);
-    ellipse(x,y,w,[h])
+  this.show = function(){
+    //this.color = img.get(this.x,this.y);
+    //this.color = random(0,255);
+    fill(this.color);
+    noStroke();
+    ellipse(this.x,this.y,this.rad * 2);
   }
+}
+
+function check(p){
+  for (var i = 0; i < points.length; i++) {
+    if(points[i].x != p.x && points[i].y != p.y){
+      //var dist = int(dist(points[i].x,points[i].y,p.x,p.y));
+      var d = (dist(points[i].x,points[i].y,p.x,p.y));
+      //print(d);
+      if(d < (points[i].rad + p.rad)){
+        return false;
+        break;
+      }
+    }
+  }
+  return true;
 }
