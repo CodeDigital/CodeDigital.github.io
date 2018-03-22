358773438f9a72be2c7a6ac29b104b4506e7366f
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-03-23 10:02:32 +1100

    Mistakes

--- projects/p5/speechtotext/sketch.js
+++ projects/p5/speechtotext/sketch.js
@@ -8,7 +8,7 @@ function preload(){
 
 function setup() {
   noCanvas();
-foo.onResult = showResult; /
+foo.onResult = showResult;
 foo.continuous = true; // boolean to set whether the speech recognition engine will give results continuously (true) or just once (false = default). bind callback function to trigger when speech is recognized
 foo.start(); // start listening
 }
