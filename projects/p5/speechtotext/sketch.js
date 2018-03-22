5df050b0a8e4cffeb15af963f59c2d38f25a5cb9
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-03-23 10:00:38 +1100

    Testing Speech Recog 2

--- projects/p5/speechtotext/sketch.js
+++ projects/p5/speechtotext/sketch.js
@@ -8,7 +8,8 @@ function preload(){
 
 function setup() {
   noCanvas();
-foo.onResult = showResult; // bind callback function to trigger when speech is recognized
+foo.onResult = showResult; /
+foo.continuous = true; // boolean to set whether the speech recognition engine will give results continuously (true) or just once (false = default). bind callback function to trigger when speech is recognized
 foo.start(); // start listening
 }
 
@@ -18,5 +19,7 @@ function draw() {
 
 function showResult()
 {
+  var test = document.getElementById("stt");
+  test.insertBefor(foo.resultString);
   print(foo.resultString); // log the result
 }
