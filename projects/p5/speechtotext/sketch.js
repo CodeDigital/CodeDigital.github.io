e20e4560e40bdb1eebb83445870622591770698d
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-03-23 12:26:28 +1100

    Continuous Interim

--- projects/p5/speechtotext/sketch.js
+++ projects/p5/speechtotext/sketch.js
@@ -9,21 +9,24 @@ function preload(){
 function setup() {
   noCanvas();
   var lang = navigator.language || 'en-US';
+
+
+  function showResult()
+  {
+    //var test = document.getElementById("stt");
+    //var txt = test.innerHTML;
+    //txt = foo.resultString + "<br>" + txt;
+    //test.innerHTML = txt;
+    createP(foo.resultString);
+    print(foo.resultString); // log the result
+  }
+
   foo = new p5.SpeechRec(lang,showResult);
-foo.continuous = true; // boolean to set whether the speech recognition engine will give results continuously (true) or just once (false = default). bind callback function to trigger when speech is recognized
-foo.interim = true;
-foo.start(); // start listening
+var continuous = true; // boolean to set whether the speech recognition engine will give results continuously (true) or just once (false = default). bind callback function to trigger when speech is recognized
+var interim = true;
+foo.start(continuous, interim); // start listening
 }
 
 function draw() {
 
-}
-
-function showResult()
-{
-  var test = document.getElementById("stt");
-  var txt = test.innerHTML;
-  txt = foo.resultString + "<br>" + txt;
-  test.innerHTML = txt;
-  print(foo.resultString); // log the result
 }
