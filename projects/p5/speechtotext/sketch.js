bf667cd74e10b617a08051f1bd741befedc7b69f
Xavier Travers <legoxavierlocketravers@gmail.com>
2018-03-23 12:10:11 +1100

    Capital 'S' Error

--- projects/p5/speechtotext/sketch.js
+++ projects/p5/speechtotext/sketch.js
@@ -9,7 +9,7 @@ function preload(){
 function setup() {
   noCanvas();
   var lang = navigator.language || 'en-US';
-  foo = new p5.speechRec(lang,showResult);
+  foo = new p5.SpeechRec(lang,showResult);
 foo.continuous = true; // boolean to set whether the speech recognition engine will give results continuously (true) or just once (false = default). bind callback function to trigger when speech is recognized
 foo.start(); // start listening
 }
