35098ff4dad9e0a9ee8ebecbdf4a98855817eaac
DigitalData <legoxavierlocketravers@gmail.com>
2019-05-14 23:10:12 +1000

    Started Navigation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//import main components
+import Navigator from './components/navigator/Navigator.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigator></Navigator>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigator/header/NavButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+const propTypes = {
+    
+};
+
+
+class NavButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+NavButton.propTypes = propTypes;
+
+
+export default NavButton;
