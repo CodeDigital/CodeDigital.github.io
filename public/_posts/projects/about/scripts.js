3afb4d3e4c32d1617fefaafce539f8a8200762f0
Xavier Travers <legoxavierlocketravers@gmail.com>
2020-04-26 18:42:13 +1000

    General Updates

--- public/_posts/projects/about/scripts.js
+++ public/_posts/projects/about/scripts.js
@@ -1,4 +1,4 @@
-var birthday = new Date();
+const birthday = new Date();
 birthday.setFullYear(2000);
 birthday.setMonth(10)
 birthday.setDate(3);
@@ -9,8 +9,14 @@ age = Math.floor(age);
 
 document.getElementById('about-age').innerText = age;
 
-var aboutAgeInterval = setInterval(function(){
-    if(document.getElementById('about-age')){
+setInterval(function(){
+    
+    var age = new Date().getTime() - birthday.getTime();
+    age /= 1000 * 60 * 60 * 24 * 365.25;
+    age = Math.floor(age);
+
+    if(document.getElementById('about-age').innerText != age){
         document.getElementById('about-age').innerText = age;
+        console.log('Age set to: ' + age);
     }
 },100);
